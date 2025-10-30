2700c8c8c4d7ca73928610aec9c1c3a12891e660
Carl Tornberg <carltornberg@hotmail.com>
2025-10-30 10:48:32 +0100

    Made function to an export instead

--- create-mint.ts
+++ create-mint.ts
@@ -1,31 +1,19 @@
-import { Keypair, Connection, PublicKey } from "@solana/web3.js";
-import { createMint, createMintToInstruction } from "@solana/spl-token";
+import { Keypair, Connection } from "@solana/web3.js";
+import { createMint } from "@solana/spl-token";
 import fs from "fs";
-import { log } from "console";
 import { homedir } from "os";
 
-const conn = new Connection("https://api.devnet.solana.com", "confirmed");
-
-await createNewMint();
-
-async function createNewMint() {
+export async function createNewMint(conn: Connection): Promise<Keypair> {
   const signer = getKeypairFromFile(homedir() + "/.config/solana/id.json");
   const mint = getKeypairFromFile(homedir() + "/.config/solana/TimeJG4zN8g3pxVJuFVdbKtcz9HMfgRDF5KrgrZ5j4i.json");
-  try {
-    const mintPubkey = await createMint(
-      conn, 
-      signer, 
-      signer.publicKey, 
-      null,
-      6,
-      mint
-    );
-    console.log("New mint created:", mintPubkey);
-  }
-  catch(e) {
-    log("Could not create mint:", e);
-  }
-  
+  await createMint(
+    conn, 
+    signer, 
+    signer.publicKey, 
+    null,
+    6,
+    mint);
+  return mint;
 }
 
 function getKeypairFromFile(path: string) {
